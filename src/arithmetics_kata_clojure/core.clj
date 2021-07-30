00ae258a494c397be3e6656552ab46a1384d950c
nawhi <25394965+nawhi@users.noreply.github.com>
2021-07-30 15:09:09 +0100

    Initial commit

---
+++ src/arithmetics_kata_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns arithmetics-kata-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/arithmetics_kata_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns arithmetics-kata-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [arithmetics-kata-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
