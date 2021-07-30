8135ef1cca393312b283b7687bba8463bb08d342
nawhi <25394965+nawhi@users.noreply.github.com>
2021-07-30 15:34:14 +0100

    Extract function for parsing operand

--- src/arithmetics_kata_clojure/core.clj
+++ src/arithmetics_kata_clojure/core.clj
@@ -1,14 +1,16 @@
 (ns arithmetics-kata-clojure.core
   (:require [clojure.string :as str]))
 
+(defn parse-op [raw] (case raw
+                       "+" +
+                       "-" -
+                       "*" *
+                       "/" /))
+
 (defn evaluate
   "Parse and evaluate an arithmetic expression"
   [raw]
   (let [tokens (str/split raw #" ")
         [_ lval operand rval _] tokens
-        op (case operand
-             "+" +
-             "-" -
-             "*" *
-             "/" /)]
+        op (parse-op operand)]
     (op (read-string lval) (read-string rval))))
