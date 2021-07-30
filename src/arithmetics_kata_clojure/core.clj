171131983474194e2a6afa9da98104cf6ab1ed25
nawhi <25394965+nawhi@users.noreply.github.com>
2021-07-30 15:36:07 +0100

    Extract another function

--- src/arithmetics_kata_clojure/core.clj
+++ src/arithmetics_kata_clojure/core.clj
@@ -7,10 +7,14 @@
                        "*" *
                        "/" /))
 
+(defn read-binop [lval operand rval]
+  (let [op (parse-op operand)]
+    (op (read-string lval) (read-string rval))))
+
 (defn evaluate
   "Parse and evaluate an arithmetic expression"
   [raw]
   (let [tokens (str/split raw #" ")
         [_ lval operand rval _] tokens
         op (parse-op operand)]
-    (op (read-string lval) (read-string rval))))
+    (read-binop lval operand rval)))
